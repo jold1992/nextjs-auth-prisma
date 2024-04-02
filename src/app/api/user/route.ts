8b05625e46d90f83fda8a446d16a8b3b7aeece5e
jlomas <jold1992@gmail.com>
2024-04-02 16:14:58 -0500

    feat: sign-up complete

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,6 +1,18 @@
 import { db } from "@/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from 'bcrypt'
+import * as z from 'zod'
+
+//Define a schema for input validation
+const userSchema = z
+  .object({
+    username: z.string().min(1, 'Username is required').max(100),
+    email: z.string().min(1, 'Email is required').email('Invalid email'),
+    password: z
+      .string()
+      .min(1, 'Password is required')
+      .min(8, 'Password must have than 8 characters'),    
+  })
 
 export async function POST(req: Request) {
 
@@ -8,7 +20,7 @@ export async function POST(req: Request) {
 
         const body = await req.json()
 
-        const { email, username, password } = body
+        const { email, username, password } = userSchema.parse(body)
 
         //check if email exists
         const existingUserByEmail = await db.user.findUnique({
@@ -38,10 +50,12 @@ export async function POST(req: Request) {
             }
         })
 
-        return NextResponse.json({ user: newUser, message: 'User created successfully'}, {status: 201})
+        const { password: newUserPassword, ...rest } = newUser
+
+        return NextResponse.json({ user: rest, message: 'User created successfully'}, {status: 201})
 
     } catch (error) {
-        //minuto 20
+        return NextResponse.json({message: "Something went wrong!!!"}, {status: 500})
     }
 
 }
